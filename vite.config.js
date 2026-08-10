8971f56910e5d3b10856bacd2057b0b455653593
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-08-10 13:24:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
   ],
-})
+});
